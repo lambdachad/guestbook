8deaedb511fcb4ed2ef87c1a39112af18cefb6b0
Odd-Harald <oddharald@myhren.ai>
2026-09-20 11:53:57 +0300

    Rename to guestbook

--- android/app/src/main/java/com/example/guestbook/MainActivity.java
+++ android/app/src/main/java/com/example/guestbook/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.example.guestbook;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from "@capacitor/cli"
 
 const config: CapacitorConfig = {
-    appId: "com.example.app",
-    appName: "app",
+    appId: "com.example.guestbook",
+    appName: "guestbook",
     webDir: "build/client",
     server: {
         androidScheme: "http",
